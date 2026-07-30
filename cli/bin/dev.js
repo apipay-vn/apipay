3cb9976b95106525162cec9f416860a4714164c5
apipay-vn <support@apipay.vn>
2026-07-30 11:32:51 +0700

    clean up

--- cli/bin/dev.js
+++ cli/bin/dev.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-// APIPAY_DASHBOARD_URL=http://localhost:5501 APIPAY_API_URL=http://localhost:5500/v1 ./bin/dev.js setup
-
 // Dev mode entry point — runs TypeScript directly
-import { execute } from '@oclif/core';
-await execute({ development: true, dir: import.meta.url });
+import {execute} from '@oclif/core';
+await execute({development: true, dir: import.meta.url});
